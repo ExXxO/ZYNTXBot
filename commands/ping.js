aa912e29a62f58c5f35e681cfad855c043d5420b
ExXxO <jan.sidler@bluewin.ch>
2021-04-24 13:54:47 +0200

    added alias functionality

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,9 @@
 module.exports = {
     name: 'ping',
+    aliases: [],
     permissions: ["ADMINISTRATOR"],
     description: 'this is a ping request!',
-    execute(zyntx, message, args){
+    execute(zyntx, message, cmd, args){
         message.channel.send('Pong!')
     }
 }
