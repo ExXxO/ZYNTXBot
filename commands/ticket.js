6db831d0e50c47e10bc71f425b2719a0d1ea03ca
ExXxO <jan.sidler@bluewin.ch>
2021-04-24 14:36:28 +0200

    ticketcreation will now mention a specific usergroup

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,3 +1,5 @@
+const config = require('../config.json');
+
 module.exports = {
     name: 'ticket',
     aliases: [],
@@ -8,7 +10,7 @@ module.exports = {
           permissionOverwrites: [
             {
               id: zyntx.user,
-              allow: ['VIEW_CHANNEL', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'SEND_MESSAGES', 'MANAGE_MESSAGES']
+              allow: ['VIEW_CHANNEL', 'MANAGE_CHANNELS', 'ADD_REACTIONS', 'SEND_MESSAGES', 'MANAGE_MESSAGES', 'MENTION_EVERYONE']
             }
           ]
         });
@@ -25,10 +27,13 @@ module.exports = {
         });
         channel.updateOverwrite(zyntx.user, {
           SEND_MESSAGE: true,
-          VIEW_CHANNEL: true
+          VIEW_CHANNEL: true,
+          MENTION_EVERYONE: true
         });
 
-        const reactionMessage = await channel.send('Danke für die Kontaktaufnahme!');
+        const ticketRole = message.guild.roles.cache.get(config.ticket_role_id)
+        
+        const reactionMessage = await channel.send(`Dein ${ticketRole} wurde erstellt. Ein Helfer wird dir bald zur Seite stehen! Stay tuned and take care.`);
 
         try {
           await reactionMessage.react("🔒");
@@ -47,16 +52,17 @@ module.exports = {
           switch (reaction.emoji.name) {
             case "🔒":
               channel.updateOverwrite(message.author, { SEND_MESSAGES: false });
+              channel.send("Das Ticket wurde geschlossen!");
               break;
             case "⛔":
-              channel.send("Deleting this channel in 5 seconds!");
-              setTimeout(() => channel.delete(), 5000);
+              channel.send("Dieser Chat wird in 60 Sekunden gelöscht!");
+              setTimeout(() => channel.delete(), 60000);
               break;
           }
         });
 
         message.channel
-      .send(`We will be right with you! ${channel}`)
+      .send(`Ticket wurde erfolgreich erstellt, wir sind bald für dich da: ${channel}!`)
       .then((msg) => {
         setTimeout(() => msg.delete(), 7000);
         setTimeout(() => message.delete(), 3000);
