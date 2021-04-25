e16cbcd9682f6b4f04ba9e1b5757b03aca8dc54c
ExXxO <jan.sidler@bluewin.ch>
2021-04-25 13:29:01 +0200

    Fix missconfigured variables for testing

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,6 @@ module.exports = {
     permissions: ["ADMINISTRATOR"],
     description: 'this is a ping request!',
     execute(message){
-        if (message.channel.id === config.ticket_create_channel_id) {
-            message.channel.send('Pong!');
-        }
+        message.channel.send('Pong!');
     }
 }
